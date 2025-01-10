9e3a0d51c526eb2ab4e74331976c529dede9a5d1
Faraaz Hussain <faraaz.husayn@gmail.com>
2025-01-11 05:07:18 +0530

    Added a default route /

--- src/app.ts
+++ src/app.ts
@@ -6,4 +6,8 @@ const app = express();
 app.use(express.json());
 app.use("/api/v1", router);
 
+app.get("/", (req, res) => {
+    res.json({ "stats endpoint": "/api/v1/stats", "deviation endpoint": "/api/v1/deviation" })
+})
+
 export default app;
